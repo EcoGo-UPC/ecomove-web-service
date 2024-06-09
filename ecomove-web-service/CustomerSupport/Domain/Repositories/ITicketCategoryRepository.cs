5f354aa8fb53c694141ff1607a71264c2f1ba350
Mily27 <milyta2703@gmail.com>
2024-06-09 01:31:17 -0500

    feat: add domain repositories

---
+++ ecomove-web-service/CustomerSupport/Domain/Repositories/ITicketCategoryRepository.cs
@@ -0,0 +1,9 @@
+using ecomove_web_service.CustomerSupport.Domain.Model.Entities;
+using ecomove_web_service.Shared.Domain.Repositories;
+
+namespace ecomove_web_service.CustomerSupport.Domain.Repositories;
+
+public interface ITicketCategoryRepository : IBaseRepository<TicketCategory>
+{
+    
+}
